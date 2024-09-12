d1446682cb552c22eb203562bbdce46af9ea96e2
Nange Sam <nangesam1@gmail.com>
2024-09-12 13:46:20 +0100

    client.js changes for deploy

--- db/client.js
+++ db/client.js
@@ -1,9 +1,11 @@
 const pg = require("pg");
+const connectionString = process.env.DATABASE_URL;
 
-const client = new pg.Client({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
+const db = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
 });
-
-module.exports = client;
+module.exports = db;
